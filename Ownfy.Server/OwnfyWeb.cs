6328e384343fea3d0e4ba3d9e6041edee5c494ea
jairov <jairo_andres_v@hotmail.com>
2016-02-14 11:14:08 -0500

    Added settings and bootstrap code in main routine

---
+++ Ownfy.Server/OwnfyWeb.cs
@@ -0,0 +1,21 @@
+﻿// <copyright company="Skivent Ltda.">
+// Copyright (c) 2013, All Right Reserved, http://www.skivent.com.co/
+// </copyright>
+
+namespace Ownfy.Server
+{
+	using Nancy;
+
+	public class OwnfyWeb : NancyModule
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OwnfyWeb"/> class.
+		/// </summary>
+		/// <param name="path">The path.</param>
+		public OwnfyWeb(string path) : base(path)
+		{
+			CodeContracts.RequiresNotNull(path);
+			//Get["/"] = _ => View["OwnfyWeb", GetModel(server)];
+		}
+	}
+}
--- Ownfy.Server/Program.cs
+++ Ownfy.Server/Program.cs
@@ -4,10 +4,36 @@
 
 namespace Ownfy.Server
 {
+	using System;
+	using System.Diagnostics;
+	using Autofac;
+	using Nancy.Hosting.Self;
+
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			using (var container = Bootstrap())
+			{
+				try
+				{
+					Trace.Listeners.Add(new ConsoleTraceListener());
+					var nancyHost = new NancyHost(new NancyBootstrapper(container), new Uri(Settings.Default.BaseAddress));
+					nancyHost.Start();
+					Console.ReadKey();
+					nancyHost.Stop();
+				}
+				catch (Exception e)
+				{
+					Trace.WriteLine("Unexpected fatal error, the server was shutdown: " + e.Message);
+				}
+			}
+		}
+
+		private static IContainer Bootstrap()
+		{
+			var builder = new ContainerBuilder();
+			return builder.Build();
 		}
 	}
 }
