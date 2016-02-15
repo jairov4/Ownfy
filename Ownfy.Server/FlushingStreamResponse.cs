d499aa89f1219d3b3ce95bc34324bce2d01767f9
jairov <jairo_andres_v@hotmail.com>
2016-02-15 08:20:17 -0500

    FlushingStreamResponse separated into new file

---
+++ Ownfy.Server/FlushingStreamResponse.cs
@@ -0,0 +1,30 @@
+﻿// <copyright company="Skivent Ltda.">
+// Copyright (c) 2013, All Right Reserved, http://www.skivent.com.co/
+// </copyright>
+
+namespace Ownfy.Server
+{
+	using System.IO;
+	using Nancy;
+
+	public class FlushingStreamResponse : Response
+	{
+		public FlushingStreamResponse(Stream sourceStream, string mimeType, int bufferSize = 16*1024)
+		{
+			this.Contents = stream =>
+			{
+				var buffer = new byte[bufferSize];
+				int read;
+				while ((read = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
+				{
+					stream.Write(buffer, 0, read);
+					stream.Flush();
+				}
+				sourceStream.Dispose();
+			};
+
+			this.StatusCode = HttpStatusCode.OK;
+			this.ContentType = mimeType;
+		}
+	}
+}
--- Ownfy.Server/OwnfyWeb.cs
+++ Ownfy.Server/OwnfyWeb.cs
@@ -4,7 +4,6 @@
 
 namespace Ownfy.Server
 {
-	using System.IO;
 	using Nancy;
 
 	public class OwnfyWeb : NancyModule
@@ -17,27 +16,6 @@ namespace Ownfy.Server
 		{
 			CodeContracts.RequiresNotNull(path);
 			this.Get["/stream/{id}"] = _ => new Response();
-		}
-	}
-
-	public class FlushingStreamResponse : Response
-	{
-		public FlushingStreamResponse(Stream sourceStream, string mimeType, int bufferSize = 16*1024)
-		{
-			this.Contents = stream =>
-			{
-				var buffer = new byte[bufferSize];
-				int read;
-				while ((read = sourceStream.Read(buffer, 0, buffer.Length)) > 0)
-				{
-					stream.Write(buffer, 0, read);
-					stream.Flush();
-				}
-				sourceStream.Dispose();
-			};
-
-			this.StatusCode = HttpStatusCode.OK;
-			this.ContentType = mimeType;
 		}
 	}
 }
