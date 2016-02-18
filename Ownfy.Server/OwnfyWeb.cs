ad68383797350733fbcf3a87dd73c8d5f745444f
jairov <jairo_andres_v@hotmail.com>
2016-02-18 00:21:47 -0500

    Added streaming range support

--- Ownfy.Server/OwnfyWeb.cs
+++ Ownfy.Server/OwnfyWeb.cs
@@ -4,6 +4,9 @@
 
 namespace Ownfy.Server
 {
+	using System;
+	using System.Globalization;
+	using System.Text.RegularExpressions;
 	using Nancy;
 	using Newtonsoft.Json;
 	using static CodeContracts;
@@ -22,7 +25,28 @@ namespace Ownfy.Server
 			this.Get["/stream/{id:int}"] = parameters =>
 			{
 				var stream = repository.GetSongStream((int)parameters.id);
-				return new FlushingStreamResponse(stream, "audio/mpeg3");
+				var res = new FlushingStreamResponse(stream, "audio/mpeg3");
+
+				var len = stream.Length;
+				foreach (var s in this.Request.Headers["Range"])
+				{
+					var start = s.Split('=')[1];
+					var m = Regex.Match(start, @"(\d+)-(\d+)?");
+					start = m.Groups[1].Value;
+					var end = len - 1;
+					if (!string.IsNullOrWhiteSpace(m.Groups[2].Value))
+					{
+						end = Convert.ToInt64(m.Groups[2].Value);
+					}
+
+					var startI = Convert.ToInt64(start);
+					var length = len - startI;
+					res.WithHeader("content-range", "bytes " + start + "-" + end + "/" + len);
+					res.WithHeader("content-length", length.ToString(CultureInfo.InvariantCulture));
+					res.StatusCode = HttpStatusCode.PartialContent;
+				}
+
+				return res;
 			};
 
 			this.Get["/artist/{artist}", true] = async (parameters, ct) =>
@@ -34,7 +58,7 @@ namespace Ownfy.Server
 			this.Get["/search/{searchText}", true] = async (parameters, ct) =>
 			{
 				var results = await repository.SearchSong((string)parameters.searchText);
-				return JsonConvert.SerializeObject(results);
+				return results;
 			};
 		}
 	}
