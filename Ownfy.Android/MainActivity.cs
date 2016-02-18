84e96f4c079d03ee341c9e71f2ab70c0447923f4
jairov <jairo_andres_v@hotmail.com>
2016-02-18 08:03:42 -0500

    Started with the UI

--- Ownfy.Android/MainActivity.cs
+++ Ownfy.Android/MainActivity.cs
@@ -11,20 +11,13 @@ namespace Ownfy.Android
 	[Activity(Label = "Ownfy.Android", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		private int count = 1;
-
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 
 			// Set our view from the "main" layout resource
 			this.SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			var button = this.FindViewById<Button>(Resource.Id.MyButton);
-
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			
 		}
 	}
 }
--- Ownfy.Android/Resources/Resource.Designer.cs
+++ Ownfy.Android/Resources/Resource.Designer.cs
@@ -60,8 +60,23 @@ namespace Ownfy.Android
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int PauseButton = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int PlayButton = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int StopButton = 2131034117;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int listView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int searchView1 = 2131034114;
 			
 			static Id()
 			{
