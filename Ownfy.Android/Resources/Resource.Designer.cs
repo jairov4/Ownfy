ef8ac6171ee2052f564ef71607daca752dbb01dd
jairov <jairo_andres_v@hotmail.com>
2016-02-18 08:17:53 -0500

    Better ui

--- Ownfy.Android/Resources/Resource.Designer.cs
+++ Ownfy.Android/Resources/Resource.Designer.cs
@@ -60,23 +60,20 @@ namespace Ownfy.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int PauseButton = 2131034116;
-			
 			// aapt resource value: 0x7f050003
-			public const int PlayButton = 2131034115;
+			public const int PauseButton = 2131034115;
 			
-			// aapt resource value: 0x7f050005
-			public const int StopButton = 2131034117;
+			// aapt resource value: 0x7f050002
+			public const int PlayButton = 2131034114;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f050004
+			public const int StopButton = 2131034116;
 			
 			// aapt resource value: 0x7f050001
 			public const int listView1 = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int searchView1 = 2131034114;
+			// aapt resource value: 0x7f050000
+			public const int searchView1 = 2131034112;
 			
 			static Id()
 			{
